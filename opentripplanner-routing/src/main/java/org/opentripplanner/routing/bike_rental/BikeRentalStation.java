38f905d5b657a690363bb91e3814320ab1c0711f
laurentg <laurent.gregoire@gmail.com>
2013-05-15 12:40:04 +0200

    Bike-rental: Add real-time data flag on station.
    This is useful for the web-service exporting the station list, as you
    can't determine otherwise if the station bikesAvailable/spacesAvailable
    values are meaningful (for static station they are the capacity / 2).

--- opentripplanner-routing/src/main/java/org/opentripplanner/routing/bike_rental/BikeRentalStation.java
+++ opentripplanner-routing/src/main/java/org/opentripplanner/routing/bike_rental/BikeRentalStation.java
@@ -17,7 +17,14 @@ public class BikeRentalStation implements Serializable {
     public int bikesAvailable;
     @XmlAttribute
     public int spacesAvailable;
-    
+
+    /**
+     * Whether this station is static (usually coming from OSM data) or a real-time source. If no real-time data, users should take
+     * bikesAvailable/spacesAvailable with a pinch of salt, as they are always the total capacity divided by two. Only the total is meaningful.
+     */
+    @XmlAttribute
+    public boolean realTimeData = true;
+
     public boolean equals(Object o) {
         if (!(o instanceof BikeRentalStation)) {
             return false;
